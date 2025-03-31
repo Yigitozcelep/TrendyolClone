2db2df2c1eec165d86572ac60c30d6e56a18b5cd
Yigit ozcelep <yoroso85onurlu@outlook.com>
2025-03-31 19:30:08 +0300

    add search bar to when outside is clicked its results diseapear

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { Command, CommandEmpty, CommandInput, CommandList, CommandItem } from './ui/command'
-import { IoIosSearch } from "react-icons/io";
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from "react";
 
 interface SearchItem {
@@ -30,15 +29,24 @@ const EmptySearch = () => {
 
 const Search = () => {
     const [value, setValue] = useState('')
-    
+    const searchDiv        = useRef<HTMLInputElement>(null);
+    const searchResultsDiv = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        document.addEventListener("mousedown", (e) => {
+            if (!searchDiv.current?.contains(e.target as Node) && !searchResultsDiv.current?.contains(e.target as Node)) {
+                searchResultsDiv.current!.style.visibility = "hidden";
+            } else searchResultsDiv.current!.style.visibility = "visible";
+        });
+    }, [])
+
     return (
-        
-        <div className='flex shrink-0 outline-0 focus-within:border-2 rounded-tl-sm rounded-tr-sm focus-within:border-orange-400 h-full z-10 items-center w-[600]'>
-            <Command className='w-full h-full'>
+        <div ref={searchDiv} className='flex shrink-0 outline-0 focus-within:outline-2 rounded-tl-sm rounded-tr-sm focus-within:outline-orange-400 h-full z-10 items-center w-[600]'>
+            <Command className='w-full h-full bg-gray-200 focus-within:bg-white'>
             <CommandInput className='outline-0 h-full w-full pl-5'  placeholder='Aradığınız ürün, kategori veya markayı yazınız' value={value} onValueChange={e => setValue(e)}/>
                 {
                     value && (
-                        <CommandList className='pl-3 absolute top-11 left-[245px] border-orange-400 bg-white border-2 rounded-bl-sm rounded-br-sm z-2 w-[600]'>
+                        <CommandList ref={searchResultsDiv} className='pl-3 absolute top-11 left-[245px] outline-orange-400 bg-white outline-2 rounded-bl-sm rounded-br-sm z-2 w-[600]'>
                         <CommandEmpty className=''>
                             <EmptySearch/>
                         </CommandEmpty> 
